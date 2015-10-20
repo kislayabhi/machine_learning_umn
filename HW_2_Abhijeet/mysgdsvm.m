5778c9450c68dac7e614fec5b7c58d8027aad0a1
kislayabhi <abhijeetkislay@gmail.com>
2015-10-20 16:05:08 -0500

    Pegasos working.Need to optimize

--- HW_2_Abhijeet/mysgdsvm.m
+++ HW_2_Abhijeet/mysgdsvm.m
@@ -1,3 +1,9 @@
+%function[] = mysgdsvm(filename, k, numruns)
+
+filename='MNIST-13.csv';
+k=15;
+numruns=1;
+
 % Pegasos: Primal Estimated sub-gradient solver for SVM
 
 % Inputs
@@ -9,8 +15,7 @@
 % Take the whole training set
 
 % Start Measuring time
-tic;
-
+data=csvread(filename);
 data_s=data(:,2:end);
 labels_s=data(:,1);
 labels_s(labels_s==3)=-1;
@@ -22,33 +27,55 @@ L=1;
 T=10000;
 
 % Set the length of the Mini-batch algorithm
-k=15;
+% k=15;
 
-% Initialize the W at the start of iteration to zero.
-W=zeros(size(data,2)-1,1);
+% Primal objective
 
-for t=1:T
+
+hold on;
+for iter=1:numruns
     
-    % Choose k data-points at Random.
-    At=randsample(size(data_s,1), k);
-    
-    % Find all the data-points in At that misclassify on the current W.
-    At_plus=At(labels_s(At).*(data_s(At,:)*W)<1);
-    
-    % Calculate the step-size
-    nt=1/(L*t);
-    
-    % Update the weight vectors.
-    Wold=W;
-    W=W*(1-nt*L)+(nt/k)*sum(repmat(labels_s(At_plus),1,length(W)).*data_s(At_plus,:))';
-    
-    % Using projection method.
-    W=min(1, 1/(L^(0.5)*norm(W)))*W;
-   
-    
-    if norm(Wold-W)<=0.1
-        break;
+    tic;
+    % Initialize the W at the start of iteration to zero.
+    W=zeros(size(data,2)-1,1);
+
+    for t=1:T
+
+        % Choose k data-points at Random.
+        At=randsample(size(data_s,1), k);
+
+        % Find all the data-points in At that misclassify on the current W.
+        At_plus=At(labels_s(At).*(data_s(At,:)*W)<1);
+
+        % Calculate the step-size
+        nt=1/(L*t);
+
+        % Update the weight vectors.
+        Wold=W;
+        W=W*(1-nt*L)+(nt/k)*sum(repmat(labels_s(At_plus),1,length(W)).*data_s(At_plus,:))';
+
+        % Using projection method.
+        W=min(1, 1/(L^(0.5)*norm(W)))*W;
+
+        %disp(sum(sign(data_s*W)~=labels_s));
+        
+        loss = 1 - (labels_s.*(data_s*W));
+        loss = loss(loss > 0);
+        primalobj(t) = L/2*(W'*W)+sum(loss);
+        
+%         loss = ones(size(labels_s)) - (labels_s.*(data_s*W));
+%         loss = sum(loss > 0);
+%         primalobj(t) = L/2*(W'*W)+loss;
+        
+        %p_obj(t)=sum(sign(data_s*W)~=labels_s)%+L*(norm(W))^2/2;
+        %plot(t,L*(norm(W))^2/2+sum(sign(data_s*W)~=labels_s),'--');
+
+        if norm(Wold-W)<=0.1
+            break;
+        end
     end
-end
+    %disp(t)
 
-toc;
+    toc;
+plot(primalobj);
+end
